2ee96f52b99a939d13fc4829b5cd4e6404547b11
Sobirov Mustakim <iammustakim01@gmail.com>
2020-10-27 16:35:50 +0800

    reshil number 1

---
+++ zadanie8/1/src/Musta/kim/Main.java
@@ -0,0 +1,16 @@
+package Musta.kim;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String result = sayHelloToUser("Mustakim");
+        System.out.println(result);
+    }
+
+    public static String sayHelloToUser(String name){
+        return "Hello " + name;
+
+    }
+}
+
+
